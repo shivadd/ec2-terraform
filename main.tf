c39bf8d60cb39cec4aa956e53dc65260f24342b3
rajulucky812 <97225776+rajulucky812@users.noreply.github.com>
2022-12-24 21:58:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "ap-southeast-2"
+  access_key = "xxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxx"
+}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-0c6120f461d6b39e9"
+  instance_type          = "t2.micro"
+  key_name               = "projecttest"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0dd290bf175c8d27f"]
+  subnet_id              = "subnet-026c58f1c05a1b7db"
+  
+  tags = {
+    Name = "terraform-instance"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
